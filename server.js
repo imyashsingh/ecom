2298e4b22b39ed2083f33e01f2e003de688450e0
imyashsingh <yash.singh5445@gmail.com>
2023-12-21 13:50:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app=express();
+dotenv.config();
+
+app.get("/",(req,res)=>{
+    res.send({message:"it running"});
+})
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,()=> console.log(`App is running on PORT=${PORT}`));
