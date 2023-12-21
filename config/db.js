dcd61b13a46a1988e5f294a2a1bdc1f3b2e66263
imyashsingh <yash.singh5445@gmail.com>
2023-12-21 16:46:19 +0530

    connected MongoDB

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDb= async ()=>{
+    try {
+        const conn=await mongoose.connect(process.env.MONGODB_URL);
+        console.log("Database is connected");
+    } catch (error) {
+        console.log(process.env.MONGODB_URL)
+        console.log(`ERROR in Database Connection=${error}`);
+
+    }
+}
+
+export default connectDb;
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import morgan from "morgan";
 
-const app=express();
+// config env
 dotenv.config();
 
+//database connection
+connectDb();
+
+const app=express();
+
+//middleware
+app.use(express.json());
+app.use(morgan('dev'))
+
+
 app.get("/",(req,res)=>{
-    res.send({message:"it running"});
+    res.send("<h1>hello world!</h1>");
 })
 
 const PORT = process.env.PORT || 8080;
