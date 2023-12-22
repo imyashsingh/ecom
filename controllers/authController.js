51d1bab2fc5233c275a8b716dae7bebd782da6ac
imyashsingh <yash.singh5445@gmail.com>
2023-12-23 02:58:04 +0530

    Adding routing for login in server

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
-import { hashPassword } from "../helpers/authHelper.js";
+import JWT from 'jsonwebtoken';
 import userModel from "../models/userModel.js";
+
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 
 export const registerController= async (req,res)=>{
 
@@ -53,8 +55,63 @@ export const registerController= async (req,res)=>{
     } catch (error) {
         console.log(error);
         res.status(500).send({
-            success : 'fail',
+            success : false,
             message : 'ERROR IN USER REGISTRATION',
+            error
+        });
+    }
+}
+
+export const loginController = async (req,res)=>{
+    try {
+        const {email,password} = req.body;
+
+        //if value not present
+        if(!email || !password){
+            res.status(404).send({
+                success : false,
+                message : 'INVALID EMAIL AND PASSWORD',
+            });
+        }
+
+        const user= await userModel.findOne({email});
+        // check email
+        if(!user){
+            res.status(404).send({
+                success : false,
+                message : 'EMAIL NOT REGISTER',
+            });
+        }
+        //check password
+        const match = await comparePassword(password,user.password);
+        if(!match){
+            res.status(404).send({
+                success : false,
+                message : 'PASSWORD NOT REGISTER',
+            });
+        }
+        
+        // jwt token
+        const token=await JWT.sign({_id:user._id},process.env.JWT_SECRET,{expiresIn : '7d',});
+
+        res.status(200).send({
+            success : true,
+            message: "LOGIN SUCCESSFULLY",
+            user : {
+                name:user.name,
+                email:user.email,
+                phone:user.phone,
+                address:user.address,
+            },
+            token,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success : false,
+            message : 'ERROR IN USER LOGIN',
+            error
         });
     }
 }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { registerController } from "../controllers/authController.js"; 
+import { loginController, registerController } from "../controllers/authController.js"; 
 
 //router object
 const router=express.Router();
@@ -8,5 +8,7 @@ const router=express.Router();
 //Routing
 //Register || methord Post
 router.post('/register',registerController);
+
+router.post('/login',loginController);
 
 export default router;
