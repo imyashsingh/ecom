1591b4de33eba6523d4ada94cda3f4d34cdb5a12
imyashsingh <yash.singh5445@gmail.com>
2023-12-24 02:19:08 +0530

    adding client side routing

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { Route,Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import HomePage from './pages/HomePage';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Policy from './pages/Policy';
+import PageNotFound from './pages/PageNotFound';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<HomePage/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/policy' element={<Policy/>}/>
+          <Route path='/*' element={<PageNotFound/>}/>
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
